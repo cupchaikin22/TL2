f5eadc338404856c7a9c9c3477f9005d85ef493e
cupchaikin22 <ftgr3AAAd3@outlook.com>
2024-12-21 12:56:07 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TL2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=16xGceArnz8");
+            System.Diagnostics.Process.Start("https://cs13.pikabu.ru/post_img/2023/08/22/11/1692734117149953530.jpg");
+            System.Diagnostics.Process.Start("https://cs13.pikabu.ru/post_img/2023/08/22/11/1692734117149953530.jpg");
+            System.Diagnostics.Process.Start("https://cs13.pikabu.ru/post_img/2023/08/22/11/1692734117149953530.jpg");
+            System.Diagnostics.Process.Start("https://cs13.pikabu.ru/post_img/2023/08/22/11/1692734117149953530.jpg");
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=16xGceArnz8");
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=16xGceArnz8");
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=16xGceArnz8");
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=16xGceArnz8");
+            System.Diagnostics.Process.Start("https://cs13.pikabu.ru/post_img/2023/08/22/11/1692734117149953530.jpg");
+            System.Diagnostics.Process.Start("https://cs13.pikabu.ru/post_img/2023/08/22/11/1692734117149953530.jpg");
+        }
+
+
+
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
